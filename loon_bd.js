46b9b5b1e9406e1f1cefa0d7ec2ab9793053cc0f
haoyangmao668 <147923064+haoyangmao668@users.noreply.github.com>
2026-04-28 08:08:27 +0800

    更新 loon_bd.js

--- loon_bd.js
+++ loon_bd.js
@@ -1,8 +1,4 @@
-/*
-同源星璃｜和小火箭Lua同算法
-无固定443、无端口判断、纯双层注入
-依靠规则隔离非443，零断网
-*/
+// 福州联通专用｜无日志｜异步非阻塞｜百度直连
 let HTTP_STATUS_INVALID = -1;
 let HTTP_STATUS_CONNECTED = 0;
 let HTTP_STATUS_WAITRESPONSE = 1;
@@ -15,19 +11,7 @@ function createVerify(address) {
     index = (index * 1318293 & 0x7FFFFFFF) + address.charCodeAt(i);
   }
   if(index < 0) index = index & 0x7FFFFFFF;
-  return `X-T5-Auth: ${index}\r\n`;
-}
-
-function rewriteHttpRequest(buf,host,port,auth){
-  let idx = buf.indexOf('/');
-  if(idx === -1) return buf;
-  let method = buf.substring(0,idx);
-  let rest = buf.substring(idx);
-  let rn = rest.indexOf("\r\n");
-  if(rn === -1) return buf;
-  let line = rest.substring(0,rn);
-  let last = rest.substring(rn);
-  return `${method}${line}\r\nHost: ${host}:${port}\r\n${auth}${last}`;
+  return index;
 }
 
 function tunnelDidConnected() {
@@ -49,27 +33,21 @@ function tunnelDidRead(data) {
     httpStatus = HTTP_STATUS_FORWARDING;
     $tunnel.established($session);
     return null;
+  } else if (httpStatus === HTTP_STATUS_FORWARDING) {
+    return data;
   }
-  return data;
 }
 
-function tunnelDidWrite(data) {
-  let host = $session.conHost;
-  let port = $session.conPort;
+function tunnelDidWrite() {
   if (httpStatus === HTTP_STATUS_CONNECTED) {
     httpStatus = HTTP_STATUS_WAITRESPONSE;
     $tunnel.readTo($session, '\x0D\x0A\x0D\x0A');
     return false;
   }
-  if(httpStatus === HTTP_STATUS_FORWARDING && data.toString().includes("HTTP/")){
-    let auth = createVerify(host);
-    return rewriteHttpRequest(data.toString(),host,port,auth);
-  }
-  return data;
+  return true;
 }
 
 function tunnelDidClose() {
-  httpStatus = HTTP_STATUS_INVALID;
   return true;
 }
 
@@ -77,6 +55,6 @@ function _writeHttpHeader() {
   let conHost = $session.conHost;
   let conPort = $session.conPort;
   let verify = createVerify(conHost);
-  let header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost: ${conHost}:${conPort}\r\nProxy-Connection: keep-alive\r\n${verify}\r\n`;
+  var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost: ${conHost}:${conPort}\r\nX-T5-Auth: ${verify}\r\nProxy-Connection: keep-alive\r\n\r\n`;
   $tunnel.write($session, header);
 }
